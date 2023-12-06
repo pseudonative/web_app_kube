8aa88728d49f79809db84132188f17d9b2e36dce
Jeremy Coando <killerpit02@yahoo.com>
2023-12-06 10:11:09 -0700

    modified rds

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "postgres_db" {
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.subnet_group.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
-
+  publicly_accessible = true
   skip_final_snapshot = true
 }
 
