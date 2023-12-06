0e62d5078056713d87885caa5a09fb4764f964b5
Jeremy Coando <killerpit02@yahoo.com>
2023-12-06 11:55:08 -0700

    added models and middleware

--- backend/main.go
+++ backend/main.go
@@ -1,36 +1,27 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/pseudonative/web_app_kube/pkg/db/database"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	r := gin.Default()
+	db := database.ConnectToDB()
+	defer db.Close()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World",
+			"message": "Hello World again",
 		})
 	})
 	r.Run()
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 }
-
-const (
-	host     = "terraform-20231206172344785100000010.cylldswjreil.us-east-1.rds.amazonaws.com"
-	port     = 5432
-	user     = "subotai"
-	password = "thefourwinds"
-	dbname   = "tulsadoom"
-)
---
+++ backend/pkg/db/database/database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectToDB() *sql.DB {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_DBNAME")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	return db
+}
