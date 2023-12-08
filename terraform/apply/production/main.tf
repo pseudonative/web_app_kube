0002e81925b4efe94025e2de361f4566ed53251e
Jeremy Coando <killerpit02@yahoo.com>
2023-12-08 12:19:50 -0700

    CICD test e2e

--- terraform/apply/production/main.tf
+++ terraform/apply/production/main.tf
@@ -5,25 +5,25 @@ module "crom_vpc" {
   }
 }
 
-module "crom_eks" {
-  source = "../../modules/eks"
-  providers = {
-    aws = aws.productionuseast1
-  }
-  vpc_id          = module.crom_vpc.vpc_id
-  private_subnets = module.crom_vpc.private_subnets
-}
+# module "crom_eks" {
+#   source = "../../modules/eks"
+#   providers = {
+#     aws = aws.productionuseast1
+#   }
+#   vpc_id          = module.crom_vpc.vpc_id
+#   private_subnets = module.crom_vpc.private_subnets
+# }
 
 
-module "crom_rds" {
-  source = "../../modules/rds"
-  providers = {
-    aws = aws.productionuseast1
-  }
+# module "crom_rds" {
+#   source = "../../modules/rds"
+#   providers = {
+#     aws = aws.productionuseast1
+#   }
 
-  db_username = "subotai"
-  db_password = "thefourwinds"
-  vpc_id      = module.crom_vpc.vpc_id
-  subnet_ids  = module.crom_vpc.public_subnets
-  db_name     = "tulsadoom"
-}
+#   db_username = "subotai"
+#   db_password = "thefourwinds"
+#   vpc_id      = module.crom_vpc.vpc_id
+#   subnet_ids  = module.crom_vpc.public_subnets
+#   db_name     = "tulsadoom"
+# }
